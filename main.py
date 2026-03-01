b5bdb19a7c1c999191e374e3328a4e558ff8e76b
Aarya Karunakaran <97793697+aarya-ak@users.noreply.github.com>
2026-03-02 03:33:16 +0530

    Implement rental house selection system

--- main.py
+++ main.py
@@ -63,3 +63,76 @@ def normalize_values(house_list, criteria_list, lower_is_better_criteria):
 
             scaled_scores[house_name][criterion] = round(normalized_value, 3)
     return scaled_scores
+
+if __name__ == "__main__":
+
+    criteria_list = ["Rent", "Distance", "Safety", "Facilities", "space"]
+    lower_is_better_criteria = ["Rent", "Distance"]
+    print("\n===== RENTAL HOUSE SELECTION SYSTEM =====")
+    number_of_houses = int(input("\nEnter number of houses: "))
+    house_list = []
+
+    for i in range(number_of_houses):
+        print(f"\nEnter details for House {i + 1}")
+        house_name = input("House name: ")
+
+        def get_value(message):
+            value = input(message)
+            return None if value.lower() == "missing" else float(value)
+
+        Rent = get_value("Monthly Rent /'missing': ")
+        Distance = get_value("Distance to Workplace (km) / 'missing': ")
+        Safety = get_value("Safety Rating 1-10 / 'missing': ")
+        Facilities = get_value("Facilities Score 1-10 /'missing': ")
+        space = get_value("Space in sq ft / 'missing': ")
+
+        house_list.append({
+            "house_name": house_name,
+            "Rent": Rent,
+            "Distance": Distance,
+            "Safety": Safety,
+            "Facilities": Facilities,
+            "space": space
+        })
+
+    print("\nEnter weights (Total must equal 10)")
+
+    weighting_criteria = {
+        "Rent": float(input("Weight for Rent: ")),
+        "Distance": float(input("Weight for Distance: ")),
+        "Safety": float(input("Weight for Safety: ")),
+        "Facilities": float(input("Weight for Facilities: ")),
+        "space": float(input("Weight for Space: "))
+    }
+
+    check_weightage_sum(weighting_criteria)
+    handle_missing_values(house_list, criteria_list)
+    scaled_scores = normalize_values(
+        house_list,
+        criteria_list,
+        lower_is_better_criteria
+    )
+    house_scores = calculate_final_scores(
+        house_list,
+        weighting_criteria,
+        scaled_scores,
+        criteria_list
+    )
+    ranked_houses = rank_houses(house_scores)
+
+    print("\n===== FINAL HOUSE RANKING =====")
+    for rank, house in enumerate(ranked_houses, start=1):
+        print(f"{rank}. {house['house_name']} → Score: {house['final_score']} / 10")
+
+    best_house = ranked_houses[0]
+    for criterion in criteria_list:
+        normalized_value = best_house["normalized_details"][criterion]
+        weight = weighting_criteria[criterion]
+        contribution = round((normalized_value * weight) / 5, 3)
+        print(f"\nCriterion: {criterion}")
+        print(f"  Weight Given: {weight}")
+        print(f"  Normalized Score: {normalized_value}")
+        print(f"  Contribution to Final Score (out of 10 scale): {contribution}")
+
+print(f"\n{best_house['house_name']} is the best option because it achieved the highest total weighted score of "
+      f"{best_house['final_score']} / 10 based on your importance levels.")
