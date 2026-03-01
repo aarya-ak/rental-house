85ae84011855a46167f49ae1dffa2a38307748c9
Aarya Karunakaran <97793697+aarya-ak@users.noreply.github.com>
2026-03-02 00:38:58 +0530

    Add handle_missing_values function to replace the missing values with the average value of that criterion.

--- main.py
+++ main.py
@@ -5,3 +5,23 @@ def check_weightage_sum(weighting_criteria):
     total_weight = sum(weighting_criteria.values())
     if total_weight != 10:
         raise ValueError(f"Total weight must be 10. \nCurrent sum = {total_weight}")
+
+def handle_missing_values(house_list, criteria_list):
+    for criterion in criteria_list:
+
+        available_values = 
+        [
+            house[criterion]
+            for house in house_list
+            if house[criterion] is not None
+        ]
+
+        average_value = 
+        (
+            sum(available_values) / len(available_values)
+            if available_values else 0
+        )
+
+        for house in house_list:
+            if house[criterion] is None:
+                house[criterion] = average_value
