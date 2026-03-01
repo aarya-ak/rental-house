f7d32a2800a814003fc6edae6a6a1cb1150672b9
Aarya Karunakaran <97793697+aarya-ak@users.noreply.github.com>
2026-03-02 00:08:09 +0530

    Add check_weightage_sum function to check whether the total weight given by the user is exactly 10.
    If the total weight given by the user is exactly not 10, then raise an error.

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 def rank_houses(scored_houses):
     return sorted(scored_houses, key=lambda x: x["final_score"], reverse=True)
+
+def check_weightage_sum(weighting_criteria):
+    total_weight = sum(weighting_criteria.values())
+    if total_weight != 10:
+        raise ValueError(f"Total weight must be 10. \nCurrent sum = {total_weight}")
