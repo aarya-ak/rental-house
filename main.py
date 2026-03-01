15b65cf5d822c8419f170e6a7a79f6203ace7cfe
Aarya Karunakaran <97793697+aarya-ak@users.noreply.github.com>
2026-03-02 02:49:57 +0530

    Implement normalization of house scores, accounting for whether lower or higher values are better.
    Added a function to normalize house scores based on specified criteria.

--- main.py
+++ main.py
@@ -41,3 +41,25 @@ def calculate_final_scores(house_list, weighting_criteria, scaled_scores, criter
             "normalized_details": scaled_scores[house_name]
         })
     return house_scores
+
+def normalize_values(house_list, criteria_list, lower_is_better_criteria):
+    scaled_scores = {}
+    for criterion in criteria_list:
+        values = [house[criterion] for house in house_list]
+        min_value = min(values)
+        max_value = max(values)
+
+        for house in house_list:
+            house_name = house["house_name"]
+            if house_name not in scaled_scores:
+                scaled_scores[house_name] = {}
+            if min_value == max_value:
+                normalized_value = 1.0
+            else:
+                if criterion in lower_is_better_criteria:
+                    normalized_value = (max_value - house[criterion]) / (max_value - min_value)
+                else:
+                    normalized_value = (house[criterion] - min_value) / (max_value - min_value)
+
+            scaled_scores[house_name][criterion] = round(normalized_value, 3)
+    return scaled_scores
